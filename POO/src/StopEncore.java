aae0f8fb8c4c5af0ac4dd8df4cbb0825d029ba7a
Ragnos <slydevis@hotmail.fr>
2014-11-19 13:16:12 +0100

    Add POO exercice 8.2

---
+++ POO/src/StopEncore.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StopEncore extends JFrame {
+    private JButton stop;
+    private JButton encore;
+    private JPanel container = new JPanel();
+
+    public StopEncore()
+    {
+        this.setTitle("Stop Encore");
+        stop = new JButton("Stop");
+        encore = new JButton("Encore");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        stop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StopEncore.this.dispose();
+
+            }
+        });
+        encore.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new StopEncore();
+
+            }
+        });
+        container.add(encore);
+        container.add(stop);
+        this.setContentPane(container);
+        this.setVisible(true);
+        this.pack();
+    }
+    public static void main(String[] args) {
+        StopEncore stopEncore = new StopEncore();
+    }
+}
