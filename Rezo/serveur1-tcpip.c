86dc1a09956ab00cc86652f21aee9cbe8eb1bf2f
ragnos <slydevis@hotmail.fr>
2014-11-21 15:01:20 +0100

    More work on serveur1-tcpip.c

--- Rezo/serveur1-tcpip.c
+++ Rezo/serveur1-tcpip.c
@@ -1,12 +1,15 @@
+#include "bor-util.h"
 // Exercice 1 :
 
-typedef enum { E_LIBRE, E_LIBRE_REQUETE, E_ECRIRE_REPONSE } Etat;
+typedef enum { E_LIBRE, E_LIRE_REQUETE, E_ECRIRE_REPONSE } Etat;
 
 typedef struct {
   Etat etat;
   int soc;
   struct sockaddr_in adr;
 } Slot;
+
+struct sockaddr_in adr_client;
 
 void init_slot (Slot *o) {
   memset(&o->adr, 0, sizeof(o->adr));
@@ -15,7 +18,7 @@ void init_slot (Slot *o) {
 }
 
 int slot_est_libre (Slot *o) {
-  if(o->etat = E_LIBRE)
+  if(o->etat == E_LIBRE)
     return 1;
 
   return -1;
@@ -26,13 +29,13 @@ void liberer_slot (Slot *o) {
     return;
 
   o->etat = E_LIBRE;
-  o->soc.close();
   init_slot(o);
 }
 
 // Exercice 2
 
-#define SLOTS_NB 32
+#define SLOTS_NB 2
+
 typedef struct {
   Slot slots[SLOTS_NB];
   int soc_ec;
@@ -40,19 +43,15 @@ typedef struct {
 } Serveur;
 
 void init_serveur(Serveur *ser) {
-  serv->soc_ec = -1;
-  for(int i = 0, i < SLOTS_NB; i++)
+  ser->soc_ec = -1;
+  for(int i = 0; i < SLOTS_NB; i++)
     init_slot(&ser->slots[i]);
-}
-
-int slot_est_libre(Slot *o) {
-  return o->etat == E_LIBRE;
 }
 
 int chercher_slot_libre(Serveur *ser) {
   for(int i = 0; i < SLOTS_NB; i++)
     if(slot_est_libre(&ser->slots[i]))
-      return ser->slot[i];
+      return i;
 
   return -1;
 }
@@ -67,17 +66,17 @@ int demarrer_serveur(Serveur *ser, int port) {
     if(bor_listen(ser->soc_ec, 8)) {
       close(ser->soc_ec);
       ser->soc_ec = -1;
-      return -1
+      return -1;
     }
 
     return 0;
 }
 void fermer_serveur(Serveur *ser) {
   close(ser->soc_ec);
-  serv->soc_ec = -1;
+  ser->soc_ec = -1;
 
-  for(int i = 0; i < SLOTS_NB, i++)
-    liberer_slot(&ser->slot[i]);
+  for(int i = 0; i < SLOTS_NB; i++)
+    liberer_slot(&ser->slots[i]);
 }
 
 // Exercice 4
@@ -98,12 +97,12 @@ int accepter_connexion(Serveur *ser) {
     return 0;
   }
 
-  printf("Serveur[%d] : connexion établie avec %s", i, bor_adrtoa_in(&adr_client));
+  printf("Serveur[%d] : connexion établie avec %s\n", i, bor_adrtoa_in(&adr_client));
 
-  Slot *o = &ser->slot[i];
+  Slot *o = &ser->slots[i];
   o->soc = soc_se;
   o->adr = adr_client;
-  o->etat = E_LIBRE_REQUETE;
+  o->etat = E_LIRE_REQUETE;
 
   return 1;
 }
@@ -111,12 +110,21 @@ int accepter_connexion(Serveur *ser) {
 // Exercice 5 :
 
 int proceder_lecture__requete(Slot *o) {
-
+	printf("Lecture en cours :\n");
+	o->etat = E_ECRIRE_REPONSE;
+	
+	char* buff;
+	bor_read_str(o->soc, buff, 20000);
+	
+	printf("%s\n", buff);
+	
+	return 0;
 }
 
 int proceder_ecriture_reponse(Slot *o) {
-
-}
+	o->etat = E_LIRE_REQUETE;
+	return 0;
+}	
 
 void traiter_slot_si_eligible(Slot*o, fd_set *set_read, fd_set *set_write) {
 
@@ -129,7 +137,7 @@ void traiter_slot_si_eligible(Slot*o, fd_set *set_read, fd_set *set_write) {
         if(FD_ISSET(o->soc, set_write))
           k = proceder_ecriture_reponse(o);
       break;
-    case E_LIBRE_REQUETE :
+    case E_LIRE_REQUETE :
         if(FD_ISSET(o->soc, set_read))
           k = proceder_lecture__requete(o);
       break;
@@ -139,7 +147,7 @@ void traiter_slot_si_eligible(Slot*o, fd_set *set_read, fd_set *set_write) {
   }
 
   if(k <= 0) {
-    printf("Serveur[%d] : libération u slot\n", o->soc);
+    printf("Serveur[%d] : libération du slot\n", o->soc);
     liberer_slot(o);
   }
 }
@@ -160,7 +168,7 @@ void preparer_select (Serveur *ser, int *maxfd, fd_set *set_read, fd_set *set_wr
     Slot *o = &ser->slots[i];
     if(slot_est_libre(o)) continue;
     switch(o->etat) {
-      case E_LIBRE_REQUETE :
+      case E_LIRE_REQUETE :
         inser_fd(o->soc, set_read, maxfd);
         break;
       case E_ECRIRE_REPONSE :
@@ -169,4 +177,59 @@ void preparer_select (Serveur *ser, int *maxfd, fd_set *set_read, fd_set *set_wr
       default : ;
     }
   }
+}
+
+int faire_scrutation(Serveur *ser) {	
+	fd_set set_read;
+	fd_set set_write;
+	int max_fd;
+
+	preparer_select(ser, &max_fd, &set_read, &set_read); 
+	
+	select(max_fd, &set_read, &set_write, NULL, NULL);
+	
+	accepter_connexion(ser);
+
+	for(int i = 0; i < SLOTS_NB; i++)
+	{
+		traiter_slot_si_eligible(&ser->slots[i], &set_read, &set_write);
+	}
+	
+	return 0;
+}
+
+Serveur ser;
+
+void gestionBoucle(int sig)
+{
+	if(sig == SIGINT)
+	{
+		printf("\nServeur : arret\n");
+		fermer_serveur(&ser);
+		exit(0);
+	}
+}
+int main(int argc, char* argv[]) {
+	if(argc - 1 != 1) {
+		printf("Erreur : Nombre d'argument invalide\n");
+		return -1;
+	}
+	
+	int numPort = atoi(argv[1]);
+		
+	struct sockaddr_in adr;
+	adr.sin_family      = AF_INET;
+	adr.sin_addr.s_addr = INADDR_ANY;
+	
+	ser.adr = adr;
+
+	bor_signal(SIGINT, gestionBoucle, SA_RESTART);
+	
+	demarrer_serveur(&ser, numPort);
+
+	while(1){
+		faire_scrutation(&ser);
+	}
+	
+	fermer_serveur(&ser);
 }
